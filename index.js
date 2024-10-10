16fc9519da1ebcf2298ac297d51b23e3bcaca242
Justin Silber <jsilber60@gmail.com>
2024-10-09 23:19:36 -0400

    added prompt questions

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: 'title',
+        message: 'What is your title?'
+    },
+    {
+        type: "input",
+        name: 'description',
+        message: 'What do you want in the description?'
+    },
+    {
+        type: "input",
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: "input",
+        name: 'usage',
+        message: 'What is the usage information?'
+    },
+    {
+        type: "input",
+        name: 'contribution',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: "input",
+        name: 'test',
+        message: 'What are your test instructions?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
