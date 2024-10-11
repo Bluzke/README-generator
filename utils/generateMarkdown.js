ea32da1ecb7f9f8300b17a888c7c18ddd7c11742
Justin Silber <jsilber60@gmail.com>
2024-10-11 10:20:46 -0400

    fixed some of the markup generated text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -56,14 +56,10 @@ function generateMarkdown(data) {
   # ${title}
 
 ## Description
-${description}
+
 Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+${description}
 
 ## Table of Contents (Optional)
 
@@ -75,12 +71,16 @@ If your README is long, add a table of contents to make it easy for users to fin
 - [License](#license)
 
 ## Installation
-${installation}
+
 What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
+${installation}
+
 ## Usage
-${usage}
+
 Provide instructions and examples for use. Include screenshots as needed.
+
+${usage}
 
 ## questions
 
@@ -103,12 +103,16 @@ Badges aren't necessary, per se, but they demonstrate street cred. Badges let ot
 If your project has a lot of features, list them here.
 
 ## How to Contribute
-${contribution}
+
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
+${usage}
+
 ## Tests
-${test}
+
 Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+${usage}
   
 
 `;
